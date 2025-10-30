57afb7f71ad90b1651b9cdfe34bf308f12119ec7
hidayatdeveloper110 <hidayatdeveloper.110@gmail.com>
2025-10-30 23:11:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+    let name= prompt("enter your nmae");   let age= prompt("enter your age");
+     alert("hello " + name + ", you are " + age + " years old.");       
+      //console.log('helo java script'); //let name=  prompt("enter your nmae");   let age= prompt("enter your age");
+      //alert("hello " + name + ", you are " + age + " years old."); 
+      
